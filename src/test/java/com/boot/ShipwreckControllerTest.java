fe196eab8b9903dd353e9c6e0b326b361dbe1004
jonayGodoy <jonaydam2@gmail.com>
2017-04-18 04:32:48 +0100

    Demo: Hamcrest Matchers

---
+++ src/test/java/com/boot/ShipwreckControllerTest.java
@@ -0,0 +1,40 @@
+package com.boot;
+
+import com.boot.controller.ShipwreckController;
+import com.boot.model.Shipwreck;
+import com.boot.repository.ShipwreckRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class ShipwreckControllerTest {
+    @InjectMocks
+    private ShipwreckController sc;
+
+    @Mock
+    private ShipwreckRepository shipwreckRepository;
+
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testShipwreckGet() throws Exception {
+        Shipwreck sw = new Shipwreck();
+        sw.setId(1L);
+        when(shipwreckRepository.findOne(1L)).thenReturn(sw);
+
+        Shipwreck wreck = sc.get(1L);
+
+        verify(shipwreckRepository).findOne(1L);
+        assertEquals(1L, wreck.getId().longValue());
+    }
+}
